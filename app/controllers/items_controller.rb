be4088a1ca1d393fbb109f0214c43992040c347f
cpan1995 <cpan1995@gmail.com>
2021-09-02 00:08:57 -0400

    commit tree 306f7e5223ad202eb8fdb0149ff75e6aa4e5c15f

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items =  user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    items = Item.find(params[:id])
+    render json: items, include: :user
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    items = Item.create(items_params)
+    render json: items, include: :user, status: :created
+  end
+
+  def render_not_found_response
+    render json: { error: "Items not found" }, status: :not_found
+  end
+
+  def items_params
+    params.permit(:name, :price, :description, :user_id)
   end
 
 end
